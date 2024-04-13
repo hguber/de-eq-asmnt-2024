dcb0baca3b1f80b5ae008e20d6394593d3ed0152
hguber <root@localhost>
2024-04-13 19:51:32 +0000

    "mage workflow"

---
+++ data_loaders/usgs_data_search.py
@@ -0,0 +1,72 @@
+import io
+import pandas as pd
+
+from time import time
+import datetime as dt
+import calendar
+from tqdm import tqdm
+
+import json
+from libcomcat.search import count, get_event_by_id, search
+from libcomcat.dataframes import get_detail_data_frame
+
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+def search_data(dt_start, dt_end, mag_start, mag_end, country, co_bb):
+
+    mag_events = search(starttime=dt_start,endtime=dt_end,
+                        minlatitude=co_bb[country]['sw']['lat'], 
+                        maxlatitude=co_bb[country]['ne']['lat'], 
+                        minlongitude=co_bb[country]['sw']['lon'], 
+                        maxlongitude=co_bb[country]['ne']['lon'],
+                        minmagnitude=mag_start, maxmagnitude=mag_end)
+    return mag_events
+
+@data_loader
+def load_data_from_api(*args, **kwargs):
+    """
+    Template for loading data from API
+    """
+    mag_start = kwargs["MAG_START"]
+    mag_end = kwargs["MAG_END"]
+    headers = kwargs["HEADER"]
+
+    #dt_end = kwargs.get("execution_date").date()
+    dt_end = dt.datetime(2024, 4, 1)
+    last_month = dt_end - dt.timedelta(days=1)
+    dt_start = dt.datetime.combine(last_month.replace(day=1), dt.datetime.min.time())
+    print(type(dt_start), type(dt_end))
+
+    event_list = []
+    eq_events = []
+
+    with open('/home/src/default_repo/json_folder/countries_bbox.json', encoding = 'utf-8') as j:
+        co_bb = json.load(j)
+    for i in tqdm(range(len(co_bb))):
+        country = list(co_bb.keys())[i]
+        try:
+            eq_events = search_data(dt_start, dt_end, mag_start, mag_end, country, co_bb)
+        except:
+            pass
+        for events in eq_events:
+            events = events.toDict()
+            event_list.append([events['id'], events['time'], events['location'], events['latitude'], events['longitude'],
+                events['depth'], events['magnitude'], events['significance'], events['alert'],
+                events['url'], events['eventtype'], country])
+
+        print('successfully appended ' + country + ': ' + str(len(event_list)))            
+                        
+    df = pd.DataFrame(event_list,columns=headers)
+    df.to_csv('/home/src/default_repo/data/eq_events.csv', encoding='utf-8')
+    return df
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
---
+++ transformers/clean_raw_data.py
@@ -0,0 +1,55 @@
+import pandas as pd
+if 'transformer' not in globals():
+    from mage_ai.data_preparation.decorators import transformer
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@transformer
+def transform(data, *args, **kwargs):
+    """
+    Template code for a transformer block.
+
+    Add more parameters to this function if this block has multiple parent blocks.
+    There should be one parameter for each output variable from each parent block.
+
+    Args:
+        data: The output from the upstream parent block
+        args: The output from any additional upstream blocks (if applicable)
+
+    Returns:
+        Anything (e.g. data frame, dictionary, array, int, str, etc.)
+    """
+    event_dtypes = {
+        'event_id' : str,
+        'location' : str,
+        'latitude' : float,
+        'longitude' : float,
+        'depth' : float,
+        'magnitude' : float,
+        'significance' : pd.Int64Dtype(),
+        'alert' : str, 
+        'url' : str,
+        'eventtype' : str,
+        'country' : str,
+    }
+
+    df = data.astype(event_dtypes)
+    df['date'] = df['datetime'].dt.date
+    df['timestamp'] = df['datetime'].dt.time
+    df = df.drop(['url','eventtype'], axis=1)
+    return df.drop_duplicates()
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
+
+@test
+def test_duplicates(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert len(output) == len(output.drop_duplicates()), 'There are duplicate rows in the dataset'
