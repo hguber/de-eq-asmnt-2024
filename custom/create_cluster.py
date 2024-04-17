1f2271a8d1010429dcf7f21738b34c7bb8dbb2fb
hguber <hgu555@gmail.com>
2024-04-17 23:37:52 +0000

    fixed typo

--- custom/create_cluster.py
+++ custom/create_cluster.py
@@ -50,7 +50,7 @@ def spark_processing(*args, **kwargs):
     # Create the job config.
     job = {
         "placement": {"cluster_name": kwargs['cluster_name']},
-        "pyspark_job": {"main_python_file_uri": 'gs://eq_spark_scripts/spark_script.py',
+        "pyspark_job": {"main_python_file_uri": 'gs://eq_spark_scripts/eq_spark_script.py',
                         "jar_file_uris" : ['gs://eq_event_jars/lib/gcs-connector-hadoop3-2.2.5.jar', 
                                            'gs://eq_event_jars/lib/spark-bigquery-with-dependencies_2.12-0.23.2.jar']},
     }
